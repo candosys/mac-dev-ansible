db92887e2dd8cbacd7b97749dd2a01f089bbeb4b
Nicholas Christopher <nchristopher@amplify.com>
2014-03-10 14:38:06 -0400

    Add sshpass build to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,24 @@
 # get python happy on both.
 #
 
-# SCRIPT_DIR=$(cd $(dirname $0) && pwd)
-# SCRIPT_NAME="${0##*/}"
-# cd ${SCRIPT_DIR}
-# 
-# SETUP_TOOLS=https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
-# 
-# echo
-# echo ${SCRIPT_NAME}:
-# echo This scripts bootstraps python and ansible and verifies other dependencies.
-# echo 
-# sudo -k
-# sudo -v -p "sudo password: "
-# curl -s ${SETUP_TOOLS} | sudo python
-# rm -rf setuptools-*
-# sudo easy_install --upgrade pip
-# sudo pip install --upgrade ansible
-# 
-# if [ -z $(which sshpass) ]; then
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+SCRIPT_NAME="${0##*/}"
+cd ${SCRIPT_DIR}
+
+SETUP_TOOLS=https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+
+echo
+echo ${SCRIPT_NAME}:
+echo This scripts bootstraps python and ansible and verifies other dependencies.
+echo 
+sudo -k
+sudo -v -p "sudo password: "
+curl -s ${SETUP_TOOLS} | sudo python
+rm -rf setuptools-*
+sudo easy_install --upgrade pip
+sudo pip install --upgrade ansible
+
+if [ -z $(which sshpass) ]; then
   echo You do not have sshpass, at least not in your path. It makes ansible easier to use but allows for sloppy ssh use.
   echo 
   read -p "Do you wish to install it? (y/n) " REPLY
@@ -36,6 +36,6 @@
     rm -rf sshpass-1.05
     popd
   fi
-# fi
+fi
 
 
